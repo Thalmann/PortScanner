ce7de312174e31ab413b18b8c80f06eea6e5c81b
Bruno Thalmann <mr.thalmann@gmail.com>
2015-10-07 14:51:50 +0200

    added nmap port scanner

---
+++ nmap_port_scanner.py
@@ -0,0 +1,32 @@
+import optparse
+import nmap
+from socket import *
+
+def nmap_scan(tgt_host, tgt_port):
+    nm_scan = nmap.PortScanner()
+    nm_scan.scan(tgt_host, tgt_port)
+    print nm_scan.all_hosts()
+    try:
+        state = nm_scan[tgt_host]['tcp'][int(tgt_port)]['state']
+    except KeyError:
+        state = nm_scan[gethostbyname(tgt_host)]['tcp'][int(tgt_port)]['state']
+    except:
+        print 'Unexpected error: ' + sys.exc_info()[0]
+    print ' [*] ' + tgt_host + ' tcp/' + tgt_port + ' ' + state
+
+def main():
+    parser = optparse.OptionParser('usage %prog -H ' + '<target host> -p <target port>')
+    parser.add_option('-H', dest='tgt_host', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgt_port', type='string', help='specify target port[s] separated by comma')
+    (options, args) = parser.parse_args()
+    tgt_host = options.tgt_host
+    tgt_ports = str(options.tgt_port).split(',')
+    print tgt_ports
+    if tgt_host is None or tgt_ports[0] is None:
+        print '[-] You must specify a target host and port[s].'
+        exit(0)
+    for tgt_port in tgt_ports:
+        nmap_scan(tgt_host, tgt_port)
+
+if __name__ == '__main__':
+    main()
